01c46e04538290f370b3ea789e859e910ccc15c1
xianwan1314 <51506030+xianwan1314@users.noreply.github.com>
2023-05-04 12:23:05 +0800

    Update aliyun.py

--- aliyun.py
+++ aliyun.py
@@ -2,6 +2,6 @@ from aligo import Aligo
 from aligo import Auth
 
 
-ali = Aligo(name='refresh_token' , refresh_token='c5fd8e0644d64516983579fefad10e0d')
+ali = Aligo(name='refresh_token' , refresh_token='4cfdb9930d974a3c9bedefc3abf4edbe')
 
-ali.upload_folder('/home/runner/Magisk/done', parent_file_id='6446693a452a7031169f475ea109ca159c1c01bc')   
+ali.upload_folder('/home/runner/Magisk/done', parent_file_id='645332b992828536213543d38e866459f2ed308e')   
